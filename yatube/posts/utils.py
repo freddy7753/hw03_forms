b04626506f6b193dc558d335d144996895a01044
Фарид Наурзгалиев <faridnaurzgaliev@gmail.com>
2022-09-15 18:09:51 +0400

    долго мучÐился сÑ related_nameÑ

--- yatube/posts/utils.py
+++ yatube/posts/utils.py
@@ -3,8 +3,8 @@ from django.core.paginator import Paginator
 NUMBER_OF_POST = 10
 
 
-def pagina(queryset, request):
+def get_paginator_obj(queryset, request):
     paginator = Paginator(queryset, NUMBER_OF_POST)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    return (page_obj)
+    return page_obj
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,14 +4,14 @@ from django.urls import reverse
 
 from .forms import PostForm
 from .models import Post, Group, User
-from .utils import pagina
+from .utils import get_paginator_obj
 
 TITLE_COUNT_SYMBOL: int = 30
 
 
 def index(request):
     post_list = Post.objects.select_related('author', 'group')
-    page_obj = pagina(post_list, request)
+    page_obj = get_paginator_obj(post_list, request)
     context = {
         'page_obj': page_obj,
     }
@@ -20,8 +20,8 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    post_list = group.posts.select_related('group')
-    page_obj = pagina(post_list, request)
+    post_list = group.posts.select_related('group', 'author')
+    page_obj = get_paginator_obj(post_list, request)
     context = {
         'group': group,
         'page_obj': page_obj,
@@ -32,10 +32,10 @@ def group_posts(request, slug):
 
 def profile(request, username):
     author = get_object_or_404(User, username=username)
-    user_posts = Post.objects.select_related(
-        'author', 'group').filter(author=author)
+    user_posts = author.posts.select_related(
+        'author', 'group')
     posts_count = Post.objects.filter(author=author).count()
-    page_obj = pagina(user_posts, request)
+    page_obj = get_paginator_obj(user_posts, request)
     context = {
         'page_obj': page_obj,
         'posts_count': posts_count,
@@ -61,29 +61,26 @@ def post_detail(request, pk):
 @csrf_exempt
 def post_create(request):
     user = get_object_or_404(User, id=request.user.pk)
-    if request.method == 'POST':
-        form = PostForm(request.POST or None)
-        if form.is_valid():
-            new_post = form.save(commit=False)
-            new_post.author = request.user
-            new_post.save()
-            return redirect(reverse('posts:profile', args=[user]))
-        return render(
-            request,
-            'posts/create_post.html', {'form': form})
-    form = PostForm()
-    return render(request, 'posts/create_post.html', {'form': form})
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        new_post = form.save(commit=False)
+        new_post.author = request.user
+        new_post.save()
+        return redirect(reverse('posts:profile', args=[user]))
+    return render(
+        request,
+        'posts/create_post.html', {'form': form})
 
 
 def post_edit(request, post_id):
-    is_edit = True
     post = get_object_or_404(Post, id=post_id)
+    if request.user != post.author:
+        return redirect(reverse('posts:post_detail', args=[post_id]))
+    is_edit = True
     form = PostForm(request.POST or None, instance=post)
     context = {
         'is_edit': is_edit, 'post': post, 'form': form
     }
-    if request.user != post.author:
-        return redirect(reverse('posts:post_detail', args=[post_id]))
     if request.method == 'POST':
         if form.is_valid:
             form.save()
