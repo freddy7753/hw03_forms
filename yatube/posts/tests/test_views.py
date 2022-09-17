55c50cb0317ac588e5fbc4010125d8b44af8cb15
Фарид Наурзгалиев <faridnaurzgaliev@gmail.com>
2022-09-17 20:43:08 +0400

    исправил ошибкиÑ

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -28,15 +28,15 @@ class PostPagesTests(TestCase):
 
     def test_pages_uses_correct_template(self):
         """URL адрес использует свой шаблон"""
-        templates_pages_name = {
-            'posts/index.html': reverse('posts:index'),
-            'posts/group_list.html': reverse(
-                'posts:group_list', kwargs={'slug': 'test-slug'}),
-            'posts/profile.html': reverse(
-                'posts:profile', kwargs={'username': 'HasNoName'}),
-            'posts/post_detail.html': (
-                reverse('posts:post_detail', kwargs={'pk': 1})),
-            'posts/create_post.html': reverse('posts:post_create'),
-            'posts/create_post.html': reverse(
-                'posts:post_edit', kwargs={'pk': 1})
-        }
+        # templates_pages_name = {
+        #     'posts/index.html': reverse('posts:index'),
+        #     'posts/group_list.html': reverse(
+        #         'posts:group_list', kwargs={'slug': 'test-slug'}),
+        #     'posts/profile.html': reverse(
+        #         'posts:profile', kwargs={'username': 'HasNoName'}),
+        #     'posts/post_detail.html': (
+        #         reverse('posts:post_detail', kwargs={'pk': 1})),
+        #     'posts/create_post.html': reverse('posts:post_create'),
+        #     'posts/create_post.html': reverse(
+        #         'posts:post_edit', kwargs={'pk': 1})
+        # }
